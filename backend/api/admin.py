1a428f987658762e7e653b5f3c6d9f9e6df31103
Дмитрий Меркулов <night-watch07@mail.ru>
2023-05-27 15:27:25 +0300

    add ingredient_model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from recipes.models import Recipe, Tag
+from recipes.models import Recipe, Tag, Ingredient
 from users.models import UserFoodgram
 
 admin.site.register(Tag)
 admin.site.register(Recipe)
 admin.site.register(UserFoodgram)
+admin.site.register(Ingredient)
+
